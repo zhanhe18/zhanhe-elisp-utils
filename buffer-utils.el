ba31458bdf53d2ab7e27696e03da929845bb2677
zhanhe <zhanhe18@gmail.com>
2021-03-25 10:43:15 +0800

    add autoload

--- buffer-utils.el
+++ buffer-utils.el
@@ -1,7 +1,26 @@
 ;;; buffer-utils.el --- description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 zhanhe
+;;
+;; Author: zhanhe <http://github/zhanhe>
+;; Maintainer: zhanhe <zhanhe18@gmail.com>
+;; Created: March 25, 2021
+;; Modified: March 25, 2021
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/zhanhe/test
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  description
+;;
+;;; Code:
+;;
 
-;;; Code :
 
+;;;###autoload
 (defun kill-buffer-regex (regex)
   "Kill buffer by REGEX."
   (interactive "sRegex : ")
@@ -12,3 +31,4 @@
 
 
 (provide 'buffer-utils)
+;;; buffer-utils.el ends here
